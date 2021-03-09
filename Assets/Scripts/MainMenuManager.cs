032d78eb7608ec1cbe7ad252761c804e7468e970
Jackson Adenan <iambatby@gmail.com>
2021-03-09 19:18:19 +1100

    Main Menu Addition

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     public string gameScene;
+    public TransitionManager transitionManager;
+
     public void Quit()
     {
         Application.Quit();
@@ -13,6 +15,13 @@ public class MainMenuManager : MonoBehaviour
 
     public void GameLoad()
     {
+        transitionManager.Transition();
+        StartCoroutine(PlayWait(1.6f));
+    }
+
+    IEnumerator PlayWait(float time)
+    {
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene(gameScene);
     }
 }
