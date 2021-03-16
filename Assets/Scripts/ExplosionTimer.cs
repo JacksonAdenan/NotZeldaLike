100740577a92c7ac6edd6ff8c9ab8b49e99672d7
Dan <daniel@ac.com.au>
2021-03-16 16:48:43 +1100

    * Working on monster explosions.

---
+++ Assets/Scripts/ExplosionTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTimer : MonoBehaviour
+{
+
+    public int explosionLength;
+    private float counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= explosionLength)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MonsterExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterExplosion : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnDestroy()
+	{
+        GameObject explosion = Instantiate(explosionEffect);
+        explosion.transform.position = this.transform.position;
+	}
+}
